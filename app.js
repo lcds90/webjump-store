4a37f23297e1b06c9aeae7d8143c16d83502d354
Leonardo Santos <lcds90@gmail.com>
2021-09-18 16:18:52 -0300

    procfile

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = require('connect')()
 var serveStatic = require('serve-static')
 var cors = require('cors');
+var port = process.env.PORT || 8888
 
 app.use(cors())
 // Serve up mock-api folder
@@ -17,6 +18,6 @@ function setJsonHeaders (res, path) {
 // Serve up public folder
 app.use('/', serveStatic('public', {'index': ['index.html', 'index.htm']}))
 
-app.listen(8888, function() {
+app.listen(port, function() {
     console.log('Acesse: http://localhost:8888')
 });
